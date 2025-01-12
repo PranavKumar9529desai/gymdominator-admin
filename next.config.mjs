73c6a8f012293ae35695b1c8df7e1d61b85f4000
PranavKumar9529desai <dpranav7745@gmail.com>
2025-01-13 01:49:49 +0530

    feat: add custom headers for favicon caching in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,20 @@ const nextConfig = {
       }
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source: '/:favicon*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
